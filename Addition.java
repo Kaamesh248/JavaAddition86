454238d8322b607ca213903e423d00e5acec35f2
karthick4763-design <karthick4763@gmail.com>
2026-07-29 16:18:08 +0530

    First Commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition{
+    public static void main(String[] args){
+        int a=18;
+        int b=17;
+        int sum=a+b;
+        System.out.println("First Number:"+a);
+        System.out.println("Second Number:"+b);
+        System.out.println("Sum:"+sum);
+    }
+}
